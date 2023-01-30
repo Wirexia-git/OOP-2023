d0069e41b9541eff883de2f70d4d92795233f472
Wirexia-git <d20124630@mytudublin.ie>
2023-01-30 23:40:22 +0000

    Hello

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -13,21 +13,7 @@ public class HelloProcessing extends PApplet
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
-		smooth();
-		
+	
 	}
 
 	float x1, y1, x2, y2;
@@ -36,6 +22,17 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-
+		stroke(255);
+		line(10, 10, 100, 100); //x1, y1, x2, y2
+		circle(300, 250, 70); //cx, cy (center x and center y), d (diameter)
+		fill(0, 255, 0);
+		rect(10, 300, 20, 100); //tlx, tly (tope left x and y), w (width), h (height)
+		stroke(127);
+		fill(0, 0, 255);
+		noStroke();
+		noFill();
+		strokeWeight(1);
+		triangle(40, 90, 300, 20, 80, 70); 
+		fill(0, 0, 255);
 	}
 }
