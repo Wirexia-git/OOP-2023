abaf9b69ae71ca5554beceaf7e3bf2bc76b70f57
Wirexia-git <d20124630@mytudublin.ie>
2023-02-02 09:09:36 +0000

    It works, but not good enough

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -1,67 +1,139 @@
+
 package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class BugZap extends PApplet
+public class BugZap extends PApplet 
 {
 
-	public void settings()
-	{
-		size(500, 500);
-	}
+    float playerX;
+    float playerY;
+    float playerWidth = 1000;
+    float playerSpeed = 30;
+	float halfPlayerWidth = playerWidth / 2;
 
-	public void setup() {
-		colorMode(HSB);
-		background(0);
+    float bugX;
+    float bugY;
+    float bugWidth = 100;
+    float halfBugWidth = bugWidth / 2;
 
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
+	int score = 0;
+    
+    public void settings()
+    {
+        size(1000, 1000);
+    }
 
-		float range = 5;
+    public void setup()
+    {
+        colorMode(RGB);
 
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
+        /*playerX = width / 2;
+        playerY = height - 50;
+        playerWidth = 50;
+        */
+        smooth();
 
-		smooth();
-		
-	}
+        resetBug();
+    }
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
-	
-	public void draw()
-	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
+    private void resetBug() 
+    {
+        bugX = random(halfBugWidth, width - halfBugWidth);
+        bugY = 50;
+    }
 
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
-		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
-		}
+    public void moveBug()
+    {
+        //bugY++;
+        bugX += random(-50, 50);
+        //System.out.println("Bug: " + bugX);
+    }
 
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
-		}
-	}
+    public void draw()
+    {
+        background(255,255,255);
+
+        drawPlayer(playerX, playerY, playerWidth);
+        drawBug(bugX, bugY, bugWidth);
+		text("Score: " + score, 350, 350);
+
+        
+        if (frameCount % 20 == 0)
+        {
+            moveBug();
+        }
+    }
+
+    public void drawBug(float x, float y, float w)
+    {
+        float halfW = w / 2;
+        stroke(0, 0, 0);
+        //noFill();
+        triangle(x - halfW, y + halfW, x, y - halfW, x + halfW, y + halfW);
+    }
+
+    void drawPlayer(float x, float y, float w)
+    {
+        float h = w/2;
+        line(x + 300, 980, x + 200, 980);
+        line(x + 300, 980, x + 300, 930);
+        line(x + 200, 980, x + 200, 930);
+        line(x + 300, 930, x + 270, 900);
+        line(x + 200, 930, x + 230, 900);
+        line(x + 270, 900, x + 230, 900);
+        line(x + 250, 900, x + 250, 870);
+    }
+
+    public void keyPressed()
+    {
+        if (keyCode == LEFT)
+        {
+            if (playerX == -200 || playerX < -200)
+            {
+                System.out.println("STOP");
+            }
+
+            else
+            {
+                System.out.println("Left arrow pressed");
+                playerX = playerX - playerSpeed;
+                System.out.println(playerX);
+            }
+        }
+
+        if (keyCode == RIGHT)
+        {
+            if (playerX == 700 || playerX > 700)
+            {
+                System.out.println("STOP");
+            }
+
+            else 
+            {
+                System.out.println("Right arrow pressed");
+                playerX = playerX + playerSpeed;
+                System.out.println(playerX);
+            }
+
+        }
+
+        if (key == ' ')
+        {
+            System.out.println("SPACE key pressed");
+
+            if (playerX + 250 > bugX - halfBugWidth && playerX + 250 < bugX + halfBugWidth)
+            {
+				line(playerX + 250, 870, playerX + 250, bugY);
+                System.out.println("Got'em");
+				score++;
+				resetBug();
+            }
+
+			else
+			{
+				line(playerX + 250, 870, playerX + 250, 0);
+			}
+        }
+    }
 }
